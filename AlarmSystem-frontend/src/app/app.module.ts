c90fd09291a6a4874dce5cd2fac3d260604dcb4d
Muhammed Fawzy <MohamedFawzy1993@gmail.com>
2019-02-23 14:08:25 +0200

    Handle Error Routing

--- AlarmSystem-frontend/src/app/app.module.ts
+++ AlarmSystem-frontend/src/app/app.module.ts
@@ -1,25 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FullLayoutComponent } from './layout/full-layout/full-layout.component';
+import {AppComponent} from './app.component';
+import {FullLayoutComponent} from './layout/full-layout/full-layout.component';
 import {RouterModule, Routes} from '@angular/router';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { StatisticsComponent } from './statistics/statistics.component';
+import {NavbarComponent} from './layout/navbar/navbar.component';
+import {StatisticsComponent} from './statistics/statistics.component';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
-import { GeneratorComponent } from './generator/generator.component';
+import {GeneratorComponent} from './generator/generator.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { AlarmsComponent } from './alarms/alarms.component';
+import {AlarmsComponent} from './alarms/alarms.component';
 import {SharedSubjectService} from './shared/shared-subject-service.service';
 import {ConfigParams} from './shared/config/config-params';
-import { CommentsComponent } from './alarms/components/comments/comments.component';
+import {CommentsComponent} from './alarms/components/comments/comments.component';
 
 //
 const coreRoutes: Routes = [
-  {path: ConfigParams.DASHBOARD_ROUTE , component: StatisticsComponent},
-  {path: ConfigParams.HOME_ROUTE , component: StatisticsComponent},
-  {path: ConfigParams.GENERATOR_ROUTE , component: GeneratorComponent},
-  {path: ConfigParams.ALARMS_ROUTE , component: AlarmsComponent}
+  {path: ConfigParams.DASHBOARD_ROUTE, component: StatisticsComponent},
+  {path: ConfigParams.HOME_ROUTE, component: StatisticsComponent},
+  {path: ConfigParams.GENERATOR_ROUTE, component: GeneratorComponent},
+  {path: ConfigParams.ALARMS_ROUTE, component: AlarmsComponent},
+  {path: '**', component: StatisticsComponent}
 ];
 
 @NgModule({
@@ -40,8 +41,9 @@ const coreRoutes: Routes = [
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [HttpClient , SharedSubjectService],
+  providers: [HttpClient, SharedSubjectService],
   bootstrap: [AppComponent]
 
 })
-export class AppModule { }
+export class AppModule {
+}
